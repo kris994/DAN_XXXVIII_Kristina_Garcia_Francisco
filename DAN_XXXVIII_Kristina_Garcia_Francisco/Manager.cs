43cf3f35550538d0db80d17799b67b2451cfeb2a
kris994 <kristipomo@gmail.com>
2020-06-24 16:00:08 +0200

    Added Barrier to check if all trucks loaded.

---
+++ DAN_XXXVIII_Kristina_Garcia_Francisco/Manager.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace DAN_XXXVIII_Kristina_Garcia_Francisco
+{
+    /// <summary>
+    /// Class that creates routes for trucks
+    /// </summary>
+    class Manager
+    {
+        #region Property
+        /// <summary>
+        /// List of all routes
+        /// </summary>
+        private List<int> allRoutes = new List<int>();
+        /// <summary>
+        /// List of routes that trucks will take
+        /// </summary>
+        public static List<int> truckRoutes = new List<int>();
+        /// <summary>
+        /// Controls the access to route files
+        /// </summary>
+        private EventWaitHandle waitHandle = new AutoResetEvent(false);
+        #endregion
+
+        /// <summary>
+        /// Reads truck routes from the list
+        /// </summary>
+        public void ReadChosenRoutes()
+        {
+            foreach (var item in truckRoutes)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine("\nThe trucks can now be loaded.\n");
+        }
+
+        /// <summary>
+        /// Creates new routes and saves them to the file
+        /// </summary>
+        public void CreateRoutes()
+        {
+            ReadWriteFile rwf = new ReadWriteFile();
+            Random rng = new Random();
+            int timeTaken = rng.Next(0, 3001);
+
+            // Wait for manager to start
+            waitHandle.WaitOne();
+            Thread.Sleep(timeTaken);
+            rwf.WriteToFile();
+
+            // Manager can continue choosing the best routes
+            waitHandle.Set();
+        }
+
+        /// <summary>
+        /// Chooses the the optimal route
+        /// </summary>
+        public void BestRoutes()
+        {
+            List<int> distinctValues = allRoutes.Distinct().ToList();
+            distinctValues.Sort();
+
+            for (int i = 0; i < distinctValues.Count; i++)
+            {
+                if (distinctValues[i] % 3 == 0)
+                {
+                    truckRoutes.Add(distinctValues[i]);
+                    if (truckRoutes.Count == 10)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.Write("\nThe best routes are: ");
+            ReadChosenRoutes();
+
+            Console.WriteLine("Loading:");
+        }
+
+        /// <summary>
+        /// Chooses the possible routes for the time given
+        /// </summary>
+        public void ChooseRoutes()
+        {
+            ReadWriteFile rwf = new ReadWriteFile();
+            // Counting how long the manager waited for values to be created
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Console.WriteLine("Manager waiting for routes to be created...");
+
+            // Sleep max 3sec       
+            waitHandle.Set();
+            waitHandle.WaitOne(3000);
+            stopWatch.Stop();
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            int elapsedTime = (ts.Seconds * 1000) + ts.Milliseconds;
+
+            // Get all routes from the file
+            rwf.ReadFile(allRoutes);
+
+            if (elapsedTime > 3000)
+            {
+                Console.WriteLine("\nToo much time has passed, manager will choose from available routes.");
+                // If too much time has passed get any 10 routes from the list
+                for (int i = 0; i < allRoutes.Count; i++)
+                {
+                    truckRoutes.Add(allRoutes[i]);
+                    if (truckRoutes.Count == 10)
+                    {
+                        break;
+                    }
+                }
+                Console.Write("\nRoutes: ");
+                ReadChosenRoutes();
+            }
+            else
+            {
+                Console.WriteLine("\nManager waited for {0} milliseconds for routes to be created.", elapsedTime);
+                // Get best 10 routes
+                BestRoutes();
+            }
+        }
+
+        /// <summary>
+        /// Creates working threads
+        /// </summary>
+        public void CreateWorkers()
+        {
+            Thread getValues = new Thread(CreateRoutes);
+            getValues.Start();
+
+            Thread menager = new Thread(ChooseRoutes);
+            menager.Start();
+
+            getValues.Join();
+            menager.Join();
+        }
+    }
+}
---
+++ DAN_XXXVIII_Kristina_Garcia_Francisco/Truck.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DAN_XXXVIII_Kristina_Garcia_Francisco
+{
+    /// <summary>
+    /// Creates trucks and represents all actions trucks have to do
+    /// </summary>
+    class Truck
+    {
+        #region Properties
+        /// <summary>
+        /// List of all active trucks
+        /// </summary>
+        private List<Thread> allTrucks = new List<Thread>();
+        /// <summary>
+        /// Saving each trucks time it took to load
+        /// </summary>
+        private Dictionary<int, string> allLoadingTime = new Dictionary<int, string>();
+        /// <summary>
+        /// Locks the trucks arrival
+        /// </summary>
+        private readonly object lockTruck = new object();
+        /// <summary>
+        /// Barrier for checking if all 10 trucks finished loading
+        /// </summary>
+        private Barrier loadingBarrier = new Barrier(10);
+        private EventWaitHandle waitTruck = new AutoResetEvent(false);
+        private EventWaitHandle waitArrivalTime = new AutoResetEvent(false);
+        /// <summary>
+        /// Generate random numbers when needed
+        /// </summary>
+        private Random rng = new Random();
+        /// <summary>
+        /// Counter increases as threads enter
+        /// </summary>
+        private int enterCounter = 0;
+        /// <summary>
+        /// Restarts the thread counts
+        /// </summary>
+        private int restartThreadCount = 0;
+        /// <summary>
+        /// Counts the amount of routes that were given to threads
+        /// </summary>
+        private int routeCounter = 0;
+        #endregion
+
+        /// <summary>
+        /// All actions that a truck needs to do
+        /// </summary>
+        public void TruckActions()
+        {
+            TruckLoading();
+            TruckRouting();
+            TruckArriving();
+        }
+
+        #region Loading
+        /// <summary>
+        /// Load 2 trucks at the same time, each loading takes a random amount of time
+        /// </summary>
+        public void TruckLoading()
+        {
+            int waitTime;
+
+            // Amount fo threads that can enter the semaphore
+            MultipleThreads(2);
+
+            Console.WriteLine("Truck {0} started loading.", Thread.CurrentThread.Name);
+
+            // Threads trying to pass
+            waitTruck.WaitOne();           
+            waitTime = rng.Next(500, 5001);
+            allLoadingTime.Add(waitTime, Thread.CurrentThread.Name);
+            // Let the next thread in
+            waitTruck.Set();
+            Thread.Sleep(waitTime);
+            // Write the time it took to load
+            if (allLoadingTime.Any(tr => tr.Value.Equals(Thread.CurrentThread.Name)))
+            {
+                Console.WriteLine("Truck {0} finished loading after {1} milliseconds."
+                    , Thread.CurrentThread.Name, allLoadingTime.FirstOrDefault(x => x.Value == Thread.CurrentThread.Name).Key);
+            }
+
+            // Let more threads enter the semaphore
+            restartThreadCount--;
+            if (restartThreadCount == 0)
+            {
+                enterCounter = 0;
+            }
+
+            // Wait all threads to finish loading
+            loadingBarrier.SignalAndWait();
+
+            // Let first truck waiting for the route pass
+            waitTruck.Set();
+        }
+        #endregion
+
+        #region Routing
+        /// <summary>
+        /// Give routes to each thread
+        /// </summary>
+        public void TruckRouting()
+        {
+            // All trucks waiting for a route
+            waitTruck.WaitOne();
+            Console.WriteLine("\t\t\t\t\t\t\t\tTruck {0} received route {1}", Thread.CurrentThread.Name, Manager.truckRoutes[routeCounter]);
+            routeCounter++;
+            // Let the next truck pass
+            waitTruck.Set();
+            // Let the first truck that needs arrival time pass
+            waitArrivalTime.Set();
+        }
+        #endregion
+
+        #region Arrival
+        /// <summary>
+        /// Calculates the trucks arrival time
+        /// </summary>
+        public void TruckArriving()
+        {
+            int arrivalTime;
+            waitArrivalTime.WaitOne();
+            arrivalTime = rng.Next(500, 5000);
+            Console.WriteLine("Truck {0} expected arrival time {1} milliseconds."
+                , Thread.CurrentThread.Name, arrivalTime);
+            enterCounter++;
+            waitArrivalTime.Set();
+            Thread.Sleep(arrivalTime);
+            ArrivalActions(arrivalTime);
+        }
+
+        /// <summary>
+        /// Depending on the time a truck took to arrive, different actions will be done
+        /// </summary>
+        /// <param name="arrivalTime">the time it took for the truck to arrive</param>
+        public void ArrivalActions(int arrivalTime)
+        {
+            lock (lockTruck)
+            {
+                if (arrivalTime > 3000)
+                {
+                    Console.WriteLine("Truck {0} delivery canceled. " +
+                    "Return time {1} milliseconds.", Thread.CurrentThread.Name, arrivalTime);
+                    Monitor.Wait(lockTruck, arrivalTime);
+                    Console.WriteLine("\t\t\t\t\t\t\t\tTruck {0} successfully returned.", Thread.CurrentThread.Name);
+                }
+                else
+                {
+                    int unloadingTime = allLoadingTime.FirstOrDefault(x => x.Value == Thread.CurrentThread.Name).Key;
+                    Console.WriteLine("Truck {0} arrived, unloading time {1} milliseconds."
+                        , Thread.CurrentThread.Name, unloadingTime / 2);
+                    Monitor.Wait(lockTruck, unloadingTime / 2);
+                    Console.WriteLine("\t\t\t\t\t\t\t\tTruck {0} successfully unloaded.", Thread.CurrentThread.Name);
+                }
+            }
+        }
+        #endregion
+
+        #region Manipulating truck threads
+        /// <summary>
+        /// Only let a fixed amount of threads to enter
+        /// </summary>
+        /// <param name="amount">the fixed amount</param>
+        public void MultipleThreads(int amount)
+        {
+            // Allow only 2 threads at the same time
+            while (true)
+            {
+                lock (lockTruck)
+                {
+                    enterCounter++;
+
+                    if (enterCounter > amount)
+                    {
+                        Thread.Sleep(0);
+                    }
+                    else
+                    {
+                        // Set for 1st thread to pass
+                        waitTruck.Set();
+                        restartThreadCount++;
+                        break;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Creates 10 different truck threads and starts them at the same time
+        /// </summary>
+        public void CreateTrucks()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Thread truckThreads = new Thread(TruckActions)
+                {
+                    Name = "Truck_" + i
+                };
+                allTrucks.Add(truckThreads);
+            }
+
+            foreach (var item in allTrucks)
+            {
+                item.Start();
+            }
+        }
+    }
+}
